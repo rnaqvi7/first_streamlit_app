489eb3aa2d51d96c70ce5d20a65ea37a36eb8dae
Ammar Raza <ammar_reza@hotmail.com>
2022-11-20 17:29:07 -0600

    Add record to snowflake table based on user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
+my_cur.execute("insert into fruit_load_list values(fruit_choice)")
+streamlit.text(f"Thanks for adding {fruit_choice}")
