0adaf564082eb8777f58ebf93247b6e5479616d3
Ammar Raza <ammar_reza@hotmail.com>
2022-11-20 18:07:02 -0600

    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def get_fruit_load_list():
   
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('{fruit_choice}')")
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values({fruit_choice})")
     return f"Thanks for adding {new_fruit}"
   
 streamlit.title('My Parents New Healthy Diner')
@@ -51,6 +51,7 @@ except URLError as e:
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.header("The fruit load list contains:")
   streamlit.dataframe(my_data_rows)
 
@@ -58,4 +59,5 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the List'):            
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(fruit_choice)
+  my_cnx.close()
   streamlit.text(back_from_function)
